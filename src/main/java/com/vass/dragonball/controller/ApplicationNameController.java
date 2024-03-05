5a1eb2913ca8a6c417bbc5e3b84576b9173bf413
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-03-04 22:40:59 -0700

    Usando meter registry

--- src/main/java/com/vass/dragonball/controller/ApplicationNameController.java
+++ src/main/java/com/vass/dragonball/controller/ApplicationNameController.java
@@ -1,5 +1,7 @@
 package com.vass.dragonball.controller;
 
+import java.util.Random;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vass.dragonball.config.DragonBallConfig;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 @RestController
 @RequestMapping("/application-name")
 public class ApplicationNameController {
+	
+	@Autowired
+	private MeterRegistry registry;
 	
 	private static final Logger log = LoggerFactory.getLogger(ApplicationNameController.class);
 
@@ -22,6 +29,8 @@ public class ApplicationNameController {
 	@GetMapping
 	public ResponseEntity<String> getAppName(){
 		log.info("Getting application name");
+		int level = new Random().nextInt(5);
+		registry.counter("vass.dragonball.name", "level", (level < 3) ? "Jr": "Sr").increment(level);
 		return ResponseEntity.ok("Hola desde "+ configuration.getName());
 	}
 
