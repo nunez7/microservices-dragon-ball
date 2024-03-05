d1bdbf2dab9f6f82632c552ee30b337c01827991
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-03-04 23:02:54 -0700

    @Timed

--- src/main/java/com/vass/dragonball/controller/ApplicationNameController.java
+++ src/main/java/com/vass/dragonball/controller/ApplicationNameController.java
@@ -5,13 +5,16 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.vass.dragonball.config.DragonBallConfig;
 
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.MeterRegistry;
 
 @RestController
@@ -27,10 +30,14 @@ public class ApplicationNameController {
 	private DragonBallConfig configuration;
 	
 	@GetMapping
+	@Timed(value = "vass.dragonball.name.get", longTask = true)
 	public ResponseEntity<String> getAppName(){
 		log.info("Getting application name");
 		int level = new Random().nextInt(5);
 		registry.counter("vass.dragonball.name", "level", (level < 3) ? "Jr": "Sr").increment(level);
+		if(level < 3) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
 		return ResponseEntity.ok("Hola desde "+ configuration.getName());
 	}
 
